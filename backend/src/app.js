cd6baf059ae8fa7f7ffaf0c4ec170d37bdc55d20
Ángel Cardoso Parreño <acp171@gcloud.ua.es>
2025-05-08 12:47:08 +0200

    seguir haciendo la base de datos

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,23 +1,40 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const fs = require('fs');
 require('dotenv').config();
 
+// Initializations
 const app = express();
 const db = require('./database');
+const sqlPath = path.join(__dirname, './database/db.sql');
+const sql = fs.readFileSync(sqlPath, 'utf8');
+
 
 // Settings
 app.set('port', process.env.PORT || 4000);
+
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
 
+
 // API
 app.use('/', require('./api/api'));
 
+
 // Static files
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+db.query(sql)
+  .then(() => {
+    console.log('Tablas creadas correctamente.');
+  })
+  .catch((err) => {
+    console.error('Error al crear las tablas:', err);
+  });
 
 // Start server
 app.listen(app.get('port'), () => {
--- backend/src/database/db.sql
+++ backend/src/database/db.sql
@@ -1,101 +1,114 @@
-CREATE DATABASE sociedad_cientifica;
-
-USE sociedad_cientifica;
-
-
 -- SOCIO_ROL TABLE --
-CREATE TABLE Socio_ROL(
-    id_socio_rol INT(100) NOT NULL AUTO_INCREMENT,
-    rol VARCHAR(16) NOT NULL,
-    CONSTRAINT PK_SOCIO_ROL PRIMARY KEY (id_socio_rol)
+CREATE TABLE IF NOT EXISTS Socio_Rol(
+    id_socio_rol SERIAL PRIMARY KEY,
+    rol VARCHAR(16) NOT NULL
 );
 
+-- TIPO_SOCIO TABLE --
+CREATE TABLE IF NOT EXISTS Tipo_Socio(
+    id_tipo_socio SERIAL PRIMARY KEY,
+    nombre_tipo VARCHAR(50) NOT NULL,
+    descripcion VARCHAR(1000) NOT NULL
+);
+
+-- SOCIO TABLE --
+CREATE TABLE IF NOT EXISTS Socio(
+    id_socio SERIAL PRIMARY KEY,
+    nombre VARCHAR(16) NOT NULL,
+    apellidos VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(60) NOT NULL,
+    telefono VARCHAR(16) NOT NULL,
+    fecha_nacimiento TIMESTAMP NOT NULL,
+    fecha_alta TIMESTAMP NOT NULL,
+    socio_rol INT NOT NULL,
+    tipo_socio INT NOT NULL,
+    CONSTRAINT FK_SOCIO_ROL_SOCIO FOREIGN KEY (socio_rol) REFERENCES Socio_Rol(id_socio_rol),
+    CONSTRAINT FK_SOCIO_TIPO_SOCIO FOREIGN KEY (tipo_socio) REFERENCES Tipo_Socio(id_tipo_socio)
+);
 
 -- DIRECCION TABLE --
-CREATE TABLE Direccion(
-    id_direcion INT(100) NOT NULL AUTO_INCREMENT,
-    calle INT(100) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS Direccion(
+    id_direccion SERIAL PRIMARY KEY,
+    calle VARCHAR(100) NOT NULL,
     ciudad VARCHAR(50) NOT NULL,
-    provincia VARCHAR(50)
-    socio INT(100) NOT NULL,
-    CONSTRAINT PK_DIRECCION PRIMARY KEY (id_direccion)
+    provincia VARCHAR(50),
+    socio INT NOT NULL,
     CONSTRAINT FK_DIRECCION_SOCIO FOREIGN KEY (socio) REFERENCES Socio(id_socio)
 );
 
-
--- SOCIO TABLE --
-CREATE TABLE Socio(
-    id_socio INT(100) NOT NULL AUTO_INCREMENT,
-    nombre VARCHAR(16) NOT NULL,
-    apellidos VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    password VARCHAR(60) NOT NULL,
-    telefono VARCHAR(16) NOT NULL,
-    fecha_nacimiento DATETIME NOT NULL,
-    fecha_alta DATETIME NOT NULL,
-    socio_rol INT(100) NOT NULL
-    CONSTRAINT PK_SOCIO PRIMARY KEY (id_socio),
-    CONSTRAINT UK_SOCIO UNIQUE (email),
-    CONSTRAINT FK_SOCIO_ROL_SOCIO FOREIGN KEY (socio_rol) REFERENCES Socio_Rol(id_socio_rol)
-);
-
-
 -- PUBLICACIONES TABLE --
-CREATE TABLE Publicaciones(
-    id_publicacion INT(100) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS Publicaciones(
+    id_publicacion SERIAL PRIMARY KEY,
     titulo VARCHAR(1000) NOT NULL,
     contenido VARCHAR(256),
-    fecha_publicacion DATETIME NOT NULL,
-    socio INT(100) NOT NULL,
-    CONSTRAINT FK_PUBLICACION_SOCIO FOREIGN KEY (socio) REFERENCES Socio(id_socio),
-    CONSTRAINT PK_PUBLICACION PRIMARY KEY (id_publicacion)
+    fecha_publicacion TIMESTAMP NOT NULL,
+    socio INT NOT NULL,
+    CONSTRAINT FK_PUBLICACION_SOCIO FOREIGN KEY (socio) REFERENCES Socio(id_socio)
 );
 
-
 -- COMENTARIO_PUBLICACION TABLE --
-CREATE TABLE Comentario_Publicacion(
-    id_comentario INT(100) NOT NULL,
+CREATE TABLE IF NOT EXISTS Comentario_Publicacion(
+    id_comentario SERIAL,
     comentario VARCHAR(256) NOT NULL,
-    socio INT(100) NOT NULL,
-    publicacion INT(100) NOT NULL,
-    fecha_comentario DATETIME NOT NULL,
-    visibilidad boolean NOT NULL,
-    CONSTRAINT PK_COMENTARIO PRIMARY KEY (id_comentario, publicacion),
+    socio INT NOT NULL,
+    publicacion INT NOT NULL,
+    fecha_comentario TIMESTAMP NOT NULL,
+    visibilidad BOOLEAN NOT NULL,
+    PRIMARY KEY (id_comentario, publicacion),
     CONSTRAINT FK_COMENTARIO_SOCIO FOREIGN KEY (socio) REFERENCES Socio(id_socio)
 );
 
-
--- LIKE PUBLICACTIONS TABLE --
-CREATE TABLE likePublication(
-    user VARCHAR(16) NOT NULL,
-    publicationID INT(100) NOT NULL,
-    CONSTRAINT PK_LIKE_PUBLICATION PRIMARY KEY (user, publicationID),
-    CONSTRAINT FK_LIKE_PUBLICATION_USERS FOREIGN KEY (user) REFERENCES users(username),
-    CONSTRAINT FK_LIKE_PUBLICATION_PUBLICATIONS FOREIGN KEY (publicationID) REFERENCES publications(id)
+-- EVENTO TABLE --
+CREATE TABLE IF NOT EXISTS Evento(
+    id_evento SERIAL PRIMARY KEY,
+    nombre_evento VARCHAR(256) NOT NULL,
+    fecha_evento_inicio TIMESTAMP NOT NULL,
+    fecha_evento_fin TIMESTAMP NOT NULL,
+    descripcion_evento VARCHAR(1000) NOT NULL,
+    direccion INT NOT NULL,
+    CONSTRAINT FK_EVENTO_DIRECCION FOREIGN KEY (direccion) REFERENCES Direccion(id_direccion)
 );
 
 
--- CHATS TABLE --
-CREATE TABLE chats(
-    id INT(100) NOT NULL AUTO_INCREMENT,
-    user1 VARCHAR(16) NOT NULL,
-    user2 VARCHAR(16) NOT NULL,
-    CONSTRAINT PK_CHATS PRIMARY KEY (id),
-    CONSTRAINT FK_CHATS_USERS1 FOREIGN KEY (user1) REFERENCES users(username),
-    CONSTRAINT FK_CHATS_USERS2 FOREIGN KEY (user2) REFERENCES users(username)
+-- INSCRIPCIONES TABLE --
+CREATE TABLE IF NOT EXISTS Inscripciones(
+    estado_inscripcion VARCHAR(256) NOT NULL,
+    evento INT NOT NULL,
+    socio INT NOT NULL,
+    PRIMARY KEY (socio, evento),
+    CONSTRAINT FK_INSCRIPCION_EVENTO FOREIGN KEY (evento) REFERENCES Evento(id_evento),
+    CONSTRAINT FK_INSCRIPCION_SOCIO FOREIGN KEY (socio) REFERENCES Socio(id_socio)    
 );
 
+-- NOTIFICACIONES TABLE --
+CREATE TABLE IF NOT EXISTS Notificaciones(
+    id_notificacion SERIAL,
+    socio INT NOT NULL,
+    mensaje VARCHAR(1000) NOT NULL,
+    fecha_envio TIMESTAMP NOT NULL,
+    estado_lectura VARCHAR(25) NOT NULL,
+    PRIMARY KEY (id_notificacion, socio),
+    CONSTRAINT FK_NOTIFICACION_SOCIO FOREIGN KEY (socio) REFERENCES Socio(id_socio)    
+);
 
--- MESSAGE CHATS TABLE --
-CREATE TABLE messageChats(
-    id INT(100) NOT NULL AUTO_INCREMENT,
-    chat INT(100) NOT NULL,
-    user VARCHAR(16) NOT NULL,
-    publication INT(100),
-    mensaje VARCHAR(1000),
-    fecha DATETIME NOT NULL,
-    CONSTRAINT PK_MESSAGE_CHATS PRIMARY KEY (id),
-    CONSTRAINT FK_MESSAGE_CHATS_USERS FOREIGN KEY (user) REFERENCES users(username),
-    CONSTRAINT FK_MESSAGE_CHATS_CHATS FOREIGN KEY (chat) REFERENCES chats(id),
-    CONSTRAINT FK_MESSAGE_CHATS_PUBLICATIONS FOREIGN KEY (publication) REFERENCES publications(id)
+-- PROYECTOS_INVESTIGACION TABLE --
+CREATE TABLE IF NOT EXISTS Proyectos_Investigacion(
+    id_proyecto SERIAL PRIMARY KEY,
+    nombre_proyecto INT NOT NULL,
+    descripcion VARCHAR(1000) NOT NULL,
+    fecha_inicio TIMESTAMP NOT NULL,
+    fecha_fin TIMESTAMP NOT NULL,
+    estado VARCHAR(25) NOT NULL
+);
+
+-- SOCIO_PROYECTO TABLE --
+CREATE TABLE IF NOT EXISTS Socio_Proyecto(
+    rol_proyecto VARCHAR(50) NOT NULL,
+    fecha_registro TIMESTAMP NOT NULL,
+    socio INT NOT NULL,
+    proyecto INT NOT NULL,
+    PRIMARY KEY (socio, proyecto),
+    CONSTRAINT FK_SOCIO_PRO_SOCIO FOREIGN KEY (socio) REFERENCES Socio(id_socio),
+    CONSTRAINT FK_SOCIO_PROYECTO_PROYECTO FOREIGN KEY (proyecto) REFERENCES Socio(id_proyecto)
 );
