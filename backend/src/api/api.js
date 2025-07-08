5300fd341677116134afd4040ba27614e5d93ca8
Ángel Cardoso Parreño <acp171@gcloud.ua.es>
2025-07-08 16:06:27 +0200

    Subir pdfs a cloudinary

--- backend/src/api/api.js
+++ backend/src/api/api.js
@@ -14,7 +14,7 @@ const SECRET_KEY = process.env.JWT_SECRET
 const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
 // Funciones privadas
-const upload = require('./uploadCloudinary');
+const upload = require('../utils/uploadCloudinary');
 const eliminarArchivoPDF = require('../utils/deleteFile');
 const { obtenernRol, obtenerSocio, obtenerSocios } = require('../utils/socioUtils');
 const { crearNotificacion, crearNotificacionEvento } = require('../utils/notificaciones');
