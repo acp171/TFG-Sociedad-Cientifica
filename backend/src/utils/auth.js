06c142c86903c3a61c5614f81171c92ab744b48e
Ángel <acp171@gcloud.ua.es>
2025-10-30 01:27:49 +0100

    Arreglar error

--- backend/src/utils/auth.js
+++ backend/src/utils/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const crypto = require('crypto');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const pool = require('../db'); // tu pool pg
 const nodemailer = require('nodemailer');
 const { RateLimiterMemory } = require('rate-limiter-flexible');
