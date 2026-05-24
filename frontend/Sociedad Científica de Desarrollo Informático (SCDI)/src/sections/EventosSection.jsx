89b21206fdfad6829df62d182766faf3221f5e01
Ángel <acp171@gcloud.ua.es>
2026-05-24 04:32:00 +0200

    Arreglado parseo y formato de las fechas de eventos en la página principal y añadida i18n al componente de EventosSection

--- frontend/Sociedad Científica de Desarrollo Informático (SCDI)/src/sections/EventosSection.jsx
+++ frontend/Sociedad Científica de Desarrollo Informático (SCDI)/src/sections/EventosSection.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
+import { useTranslation } from "react-i18next";
 
 const EventosSection = () => {
+    const { t } = useTranslation();
     const [eventos, setEventos] = useState([]);
     const [loading, setLoading] = useState(true);
 
@@ -19,18 +21,22 @@ const EventosSection = () => {
 
     return (
         <section className="py-16 px-6 max-w-6xl mx-auto text-center">
-        <h2 className="text-3xl font-bold mb-8">Próximos Eventos</h2>
+        <h2 className="text-3xl font-bold mb-8">{t("home.proximos_eventos")}</h2>
 
         {loading ? (
-            <p className="text-gray-500">Cargando eventos...</p>
+            <p className="text-gray-500">{t("home.cargando_eventos")}</p>
         ) : eventos.length === 0 ? (
-            <p className="text-gray-500">No hay eventos próximos.</p>
+            <p className="text-gray-500">{t("home.no_eventos")}</p>
         ) : (
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-10">
                 {eventos.slice(0, 4).map(evento => (
                     <div key={evento.id_evento} className="border p-4 rounded-lg">
                         <span className="block font-bold text-lg">{evento.nombre_evento}</span>
-                        <span className="text-gray-600">{evento.fecha_evento_inicio} - {evento.fecha_evento_fin}</span>
+                        <span className="text-gray-600 block text-sm mt-2">
+                            {new Date(evento.fecha_evento_inicio).toLocaleString()} <br/>
+                            {t("home.hasta")} <br/>
+                            {new Date(evento.fecha_evento_fin).toLocaleString()}
+                        </span>
                     </div>
                 ))}
             </div>          
