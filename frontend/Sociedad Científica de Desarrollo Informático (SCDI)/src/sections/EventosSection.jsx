6608138b4091c51a925623f16e5de7acb676a386
Ángel <acp171@gcloud.ua.es>
2026-06-02 18:21:05 +0200

    Arreglado error con la configuración del backend al reorganizar los ficheros

--- frontend/Sociedad Científica de Desarrollo Informático (SCDI)/src/sections/EventosSection.jsx
+++ frontend/Sociedad Científica de Desarrollo Informático (SCDI)/src/sections/EventosSection.jsx
@@ -1,4 +1,4 @@
-import API_BASE_URL from '../../config/backendConfig';
+import API_BASE_URL from '../config/backendConfig';
 import { useEffect, useState } from 'react';
 import { useTranslation } from "react-i18next";
 
@@ -9,39 +9,39 @@ const EventosSection = () => {
 
     useEffect(() => {
         fetch(`${API_BASE_URL}/listado-eventos-cientificos`)
-        .then((res) => res.json())
-        .then((data) => {
-            setEventos(data.eventos.listaEventos || []);
-            setLoading(false);
-        })
-        .catch((err) => {
-            console.error('Error al obtener eventos:', err);
-            setLoading(false);
-        });
+            .then((res) => res.json())
+            .then((data) => {
+                setEventos(data.eventos.listaEventos || []);
+                setLoading(false);
+            })
+            .catch((err) => {
+                console.error('Error al obtener eventos:', err);
+                setLoading(false);
+            });
     }, []);
 
     return (
         <section className="py-16 px-6 max-w-6xl mx-auto text-center">
-        <h2 className="text-3xl font-bold mb-8">{t("home.proximos_eventos")}</h2>
+            <h2 className="text-3xl font-bold mb-8">{t("home.proximos_eventos")}</h2>
 
-        {loading ? (
-            <p className="text-gray-500">{t("home.cargando_eventos")}</p>
-        ) : eventos.length === 0 ? (
-            <p className="text-gray-500">{t("home.no_eventos")}</p>
-        ) : (
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-10">
-                {eventos.slice(0, 4).map(evento => (
-                    <div key={evento.id_evento} className="border p-4 rounded-lg">
-                        <span className="block font-bold text-lg">{evento.nombre_evento}</span>
-                        <span className="text-gray-600 block text-sm mt-2">
-                            {new Date(evento.fecha_evento_inicio).toLocaleString()} <br/>
-                            {t("home.hasta")} <br/>
-                            {new Date(evento.fecha_evento_fin).toLocaleString()}
-                        </span>
-                    </div>
-                ))}
-            </div>          
-        )}
+            {loading ? (
+                <p className="text-gray-500">{t("home.cargando_eventos")}</p>
+            ) : eventos.length === 0 ? (
+                <p className="text-gray-500">{t("home.no_eventos")}</p>
+            ) : (
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-10">
+                    {eventos.slice(0, 4).map(evento => (
+                        <div key={evento.id_evento} className="border p-4 rounded-lg">
+                            <span className="block font-bold text-lg">{evento.nombre_evento}</span>
+                            <span className="text-gray-600 block text-sm mt-2">
+                                {new Date(evento.fecha_evento_inicio).toLocaleString()} <br />
+                                {t("home.hasta")} <br />
+                                {new Date(evento.fecha_evento_fin).toLocaleString()}
+                            </span>
+                        </div>
+                    ))}
+                </div>
+            )}
         </section>
     );
 };
