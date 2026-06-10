392cf080671319d61232eda72094fb80a2b7d8b0
Ángel Cardoso Parreño <acp171@gcloud.ua.es>
2026-06-10 02:43:17 +0200

    Arreglar fallo por vulnerabilidad con la biblioteca cloudinary

--- backend/src/utils/uploadCloudinary.js
+++ backend/src/utils/uploadCloudinary.js
@@ -1,16 +1,16 @@
 const CloudinaryStorageObj = require('multer-storage-cloudinary');
 const CloudinaryStorage = CloudinaryStorageObj.CloudinaryStorage || CloudinaryStorageObj;
 const multer = require('multer');
-const cloudinaryV2 = require('cloudinary').v2;
+const cloudinary = require('cloudinary');
 
-cloudinaryV2.config({
+cloudinary.v2.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
-    cloudinary: cloudinaryV2,
+    cloudinary: cloudinary,
     params: {
         folder: 'articulos',
         resource_type: 'raw',
